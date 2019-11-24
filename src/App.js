94b43f0bdf88c3b6a5835799c662171d0228e4aa
Manasa Hari <harimanasa17@gmail.com>
2019-11-24 13:31:28 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
+import SignUpForm from './pages/SignUpForm';
+import SignInForm from './pages/SignInForm';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router basename="/DeCentVote/">
+        <div className="App">
+          <div className="App__Aside"></div>
+          <div className="App__Form">
+            <div className="PageSwitcher">
+                {/* Added by Manasa on 22 November 2019: For the top right selection pane */}
+                <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+                <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+              </div>
+
+              {/* Added by Manasa on 22 November 2019: For main center selection pane */}
+              <div className="FormTitle">
+                  <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+              </div>
+
+              <Route exact path="/" component={SignUpForm}>
+              </Route>
+              <Route path="/sign-in" component={SignInForm}>
+              </Route>
+          </div>
+
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
